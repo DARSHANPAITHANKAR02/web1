499d7d5b84e836f54bebbfdbcf5dc6168076876e
ADWAIT <adwaitnsk.2009@rediffmail.com>
2020-08-30 14:07:58 +0530

    authoriztion

--- Hostel_finder/Find_Hostel/urls.py
+++ Hostel_finder/Find_Hostel/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("hostels/<int:myid>", views.hostels, name="Hostels"),
     path("contact/", views.contact, name="ContactUs"),
     path("about/", views.aboutus, name="AboutUs"),
-    path("register/", views.register, name="Register"),
+    path("register", views.register, name="Register"),
     path("search/", views.search, name="search"),
-    
-]
+    path("login", views.signin, name="login"),
+    path("logout", views.signout, name="logout")
+    ]
--- Hostel_finder/Find_Hostel/views.py
+++ Hostel_finder/Find_Hostel/views.py
@@ -1,56 +1,101 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Hostels,Contact
+from .models import Hostels, Contact
 from math import ceil
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 def index(request):
-    host=Hostels.objects.all()
+    host = Hostels.objects.all()
     print(host)
-    
-    n=len(host)
+
+    n = len(host)
     print(n)
-    
-    params={ 'range':range(0,n),'Hostel':host}
-    return render(request,'Find_Hostel/index.html',params)
+
+    params = {'range': range(0, n), 'Hostel': host}
+    return render(request, 'Find_Hostel/index.html', params)
+
 
 def aboutus(request):
-    
-    return render(request,'Find_Hostel/about.html')
+
+    return render(request, 'Find_Hostel/about.html')
 
 # def hostels(request):
 #     return render(request,'Find_Hostel/about.html')
 
+
 def contact(request):
-    if request.method=="POST":
+    if request.method == "POST":
         print(request)
-        name= request.POST.get('name', '')
-        email=request.POST.get('email', '')
-        phone=request.POST.get('phone', '')
-        desc=request.POST.get('desc', '')
-        contact=Contact(name=name,email=email,phone=phone,desc=desc)
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save
-    return render(request,'Find_Hostel/contact.html')
+    return render(request, 'Find_Hostel/contact.html')
 
 
 def register(request):
-    return render(request,'Find_Hostel/register.html')
+    if request.method == "POST":
+        name = request.POST['name']
+        fname = request.POST['fname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        new_user = User.objects.create_user(name, email, password1)
+        new_user.fname = fname
+        new_user.save()
+        messages.success(request, "user succcessfully created")
+        return redirect("HostelHome")
+
+    else:
+        return HttpResponse("404 NOT FOUND")
+
 
 def search(request):
-    return render(request,'Find_Hostel/about.html')
+    return render(request, 'Find_Hostel/about.html')
 
 
-def hostels(request,myid):
+def hostels(request, myid):
     # fetch product using id
-    host=Hostels.objects.filter(id=myid)
+    host = Hostels.objects.filter(id=myid)
     print(host)
-    
-    n=len(host)
-    print(n)
-    
-    params={'Hostel':host[0]}
-   
-    
-    return render(request,'Find_Hostel/hostels.html',params)
 
+    n = len(host)
+    print(n)
+
+    params = {'Hostel': host[0]}
+
+    return render(request, 'Find_Hostel/hostels.html', params)
+
+
+def signin(request):
+    if request.method == "POST":
+        loginname = request.POST['loginname']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username=loginname, password=loginpassword)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "logged in successfully")
+            return redirect("HostelHome")
+        else:
+            messages.error(request, "invalid credentials")
+            return redirect("HostelHome")
+    else:
+        return HttpResponse("404 NOT FOUND")
+
+
+def signout(request):
+    if request.method == "POST":
+        logout(request)
+        messages.success(request, "logged out")
+        return redirect("HostelHome")
+    else:
+        return HttpResponse("404 NOT FOUND")
